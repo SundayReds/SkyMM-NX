af14a7fd96def91f4769a7d59b995f2fd0f76aca
SundayReds <sunday.vermillions@gmail.com>
2021-09-26 15:40:02 +0800

    Singleton fix for AliasManager, update naming convention, minor fixes

--- include/alias_manager.hpp
+++ include/alias_manager.hpp
@@ -7,37 +7,38 @@
 #include <vector>
 
 #include "file_helper.hpp"
+#include "path_helper.hpp"
 #include "string_helper.hpp"
 
-#define SEPARATOR_TOKEN ","
+#define SEPARATOR_TOKEN "," // token must be illegal in filenames
 #define SPACE " "
 #define NEWLINE "\n"
 
-class alias_manager {
+class AliasManager {
 
 public:
-    static alias_manager *getInstance();
+    static AliasManager *get_instance();
 
     bool has_alias(std::string filename);
     std::string get_alias(std::string filename);
     void set_alias(std::string filename, std::string alias);
     void remove_alias(std::string filename);
-    void load_saved_alias(std::string filepath);
-    void save_alias_list_to_disk(std::string dest);
+    void load_saved_alias(std::string filepath=SKYMM_NX_ALIAS_TXT_FILE);
+    void save_alias_list_to_disk(std::string dest=SKYMM_NX_ALIAS_TXT_FILE);
     
 
 private:
-    static alias_manager *instance;
+    static AliasManager *instance;
     std::unordered_map<std::string, std::string> filename_to_alias_mapping;
 
-    alias_manager();
+    AliasManager();
 
-    class alias_parser {
+    class AliasParser {
     public:
         void parse(std::string text);
         std::string convert_to_text(std::unordered_map<std::string, std::string> &alias_list);
     };
 
-    alias_parser parser;
+    AliasParser parser;
 
 };
--- src/alias_manager.cpp
+++ src/alias_manager.cpp
@@ -1,59 +1,70 @@
 #include "alias_manager.hpp"
 
-alias_manager::alias_manager() {
-    parser = alias_manager::alias_parser();
+AliasManager* AliasManager::instance = 0;
+
+AliasManager::AliasManager() {
+    parser = AliasManager::AliasParser();
 }
 
-alias_manager* alias_manager::getInstance() {
+AliasManager* AliasManager::get_instance() {
     if (!instance) {
-        instance = new alias_manager();
+        instance = new AliasManager();
     }
     return instance;
 }
 
-bool alias_manager::has_alias(std::string filename) {
+bool AliasManager::has_alias(std::string filename) {
     return this->filename_to_alias_mapping.find(filename) == this->filename_to_alias_mapping.end();
 }
 
-std::string alias_manager::get_alias(std::string filename) {
+std::string AliasManager::get_alias(std::string filename) {
     return has_alias(filename) ? filename_to_alias_mapping.at(filename)
                                     : std::string();
 }
 
-void alias_manager::set_alias(std::string filename, std::string alias) {
+void AliasManager::set_alias(std::string filename, std::string alias) {
+    // programmer error, empty base_name passed in
+    if (filename.empty()) {
+        return;
+    }
+
+    // remove alias
     if (alias.empty()) {
         remove_alias(filename);
         return;
     }
 
-    if (!filename.empty()) {
-        filename_to_alias_mapping[filename] = alias;
+    // alias remains the same
+    if (this->has_alias(filename) 
+        && this->filename_to_alias_mapping.at(filename) == alias) {
+        return;
     }
+
+    // otherwise, change the alias and rewrite saved txt file
+    filename_to_alias_mapping[filename] = alias;
+    this->save_alias_list_to_disk();
 }
 
-void alias_manager::remove_alias(std::string filename) {
+void AliasManager::remove_alias(std::string filename) {
     if (has_alias(filename)) {
         this->filename_to_alias_mapping.erase(filename);
     }
 }
 
-void alias_manager::load_saved_alias(std::string filepath) {
-    //try {
+void AliasManager::load_saved_alias(std::string filepath) {
+    // maybe print relevant message if loading fails
     if (!this->filename_to_alias_mapping.empty()) {
         this->filename_to_alias_mapping.clear();
     }
 
     std::string saved_text;
     saved_text = file_helper::get_file_as_string(filepath);
-    this->parser.parse(saved_text);
-    //} catch (std::exception& e) {
-        // TODO: print relevant console stuff. 
-        // alias is not core functionality,
-        // so just catch all file handling exceptions here.
-    //}
+    if (!saved_text.empty()) {
+        this->parser.parse(saved_text);
+    }
 }
 
-void alias_manager::alias_parser::parse(std::string text) {
+void AliasManager::AliasParser::parse(std::string text) {
     std::stringstream buffer = std::stringstream(text);
 
     std::vector<std::string> entries = split(text, NEWLINE);
@@ -65,20 +76,28 @@ void alias_manager::alias_parser::parse(std::string text) {
             tokens.at(1).empty()) {
             continue;
         }
-        alias_manager::getInstance()->set_alias(tokens.at(0), tokens.at(1));
+
+        std::string mod_name = tokens.at(0);
+        std::string alias = tokens.at(1);
+
+        // for the case when user uses a separator token in alias
+        if (tokens.size() > 2) {
+            for (unsigned long i = 2; i < tokens.size(); i++) {
+                alias += tokens.at(i);
+            }
+        }
+
+        AliasManager::get_instance()->set_alias(mod_name, alias);
     }
 }
 
-void alias_manager::save_alias_list_to_disk(std::string dest) {
-    //try {
+void AliasManager::save_alias_list_to_disk(std::string dest) {
+    // maybe print relevant console message if saving fails
     std::string plaintext = this->parser.convert_to_text(this->filename_to_alias_mapping);
     file_helper::save_string_to_file(dest, plaintext);
-    //} catch (std::exception &e) {
-        // TODO print relevant error message on console
-    //}
 }
 
-std::string alias_manager::alias_parser::convert_to_text(std::unordered_map<std::string, std::string> &alias_list) {
+std::string AliasManager::AliasParser::convert_to_text(std::unordered_map<std::string, std::string> &alias_list) {
     if (alias_list.empty()) {
         return std::string();
     }
