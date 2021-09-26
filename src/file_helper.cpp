f3116477c87890170fa3da63af6120e64296a0bc
SundayReds <sunday.vermillions@gmail.com>
2021-09-26 15:29:31 +0800

    add file checks before attempting to read from txt file

--- src/file_helper.cpp
+++ src/file_helper.cpp
@@ -1,12 +1,16 @@
 #include "file_helper.hpp"
 
 std::string file_helper::get_file_as_string(std::string filepath) {
-    std::stringstream buffer;
     std::ifstream t(filepath);
 
-    buffer << t.rdbuf();
-    t.close();
-    return buffer.str();
+    if (t) {
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+        t.close();
+        return buffer.str();
+    }
+
+    return std::string();
 }
 
 void file_helper::save_string_to_file(std::string dest, std::string text) {
